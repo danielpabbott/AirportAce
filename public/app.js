73097770db4d175a2de06f01853b158c7ac6e9c8
Daniel Abbott <danielpabbott@gmail.com>
2016-05-17 09:21:58 -0600

    first commit

---
+++ public/app.js
@@ -0,0 +1,7 @@
+$(document).ready()
+
+// populate airport list from API
+
+// request selected airport arrivals over specified time period
+
+// put data into specified windows and print busiest times plus graph of specified time period
